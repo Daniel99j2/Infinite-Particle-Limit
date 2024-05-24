faad7f76590bd9bd40adfce5e5df1b1d9c61746a
Daniel99j2 <daniel99j1235@proton.me>
2024-05-25 07:53:49 +0800

    Initial commit

---
+++ src/client/java/com/daniel99j/infparticlelimit/InfiniteParticleLimitClient.java
@@ -0,0 +1,10 @@
+package com.daniel99j.infparticlelimit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfiniteParticleLimitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/daniel99j/infparticlelimit/InfiniteParticleLimitDataGenerator.java
@@ -0,0 +1,11 @@
+package com.daniel99j.infparticlelimit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfiniteParticleLimitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
